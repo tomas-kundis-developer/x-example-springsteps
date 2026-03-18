53fcc5a62956f166286cfb2deb301a812edb4e79
Springsteps Developer <springsteps@example.org>
2026-03-18 01:49:38 +0100

    REFACT: Resolve Checkstyle issues.

--- src/main/java/org/springsteps/Application.java
+++ src/main/java/org/springsteps/Application.java
@@ -3,9 +3,17 @@ package org.springsteps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main Spring Boot application class.
+ */
 @SpringBootApplication
 public class Application {
 
+  /**
+   * Spring Boot starts here.
+   *
+   * @param args Java command-line arguments.
+   */
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }

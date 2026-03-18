d42c81ba2fb6d49ead8fffd437057d026d39f87f
Springsteps Developer <springsteps@example.org>
2026-03-18 04:28:44 +0100

    DB: Add Inquiry development data.

---
+++ src/main/resources/data-postgresql.sql
@@ -0,0 +1,9 @@
+-- Few example inquiries for development environment.
+
+INSERT INTO inquiries
+VALUES (DEFAULT, 'Foo Bar', 'foo.bar@example.org', '00421 907 444 333',
+        'Development inquiry #1.');
+
+INSERT INTO inquiries
+VALUES (DEFAULT, 'James Smith', 'james.smith@example.org', '+420907444222#8',
+        'Development inquiry #2.');
